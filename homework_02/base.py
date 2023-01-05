e39311f0954d44c8524f15871b427bc29e02a43a
lisanders <denis-krv93@yandex.ru>
2023-01-05 20:11:09 +0300

    add solution for homework_02

--- homework_02/base.py
+++ homework_02/base.py
@@ -1,5 +1,31 @@
 from abc import ABC
+from homework_02.exceptions import LowFuelError, NotEnoughFuel
 
 
 class Vehicle(ABC):
-    pass
+    weight = 2000
+    started = False
+    fuel = 60
+    fuel_consumption = 12
+
+    def __init__(self, weight, fuel, fuel_consumption):
+        self.weight = weight
+        self.fuel = fuel
+        self.fuel_consumption = fuel_consumption
+
+    def start(self):
+        if not self.started:
+            if self.fuel > 0:
+                self.started = True
+            else:
+                raise LowFuelError("Топливо на нуле!")
+        return self.started
+
+    def move(self, distance):
+        distance_cost = self.fuel_consumption * distance
+        if self.fuel >= distance_cost:
+            self.fuel -= distance_cost
+        else:
+            raise NotEnoughFuel("Топлива не достаточно для преодоления данной дистанции")
+
+
--- homework_02/plane.py
+++ homework_02/plane.py
@@ -1,3 +1,23 @@
 """
 создайте класс `Plane`, наследник `Vehicle`
 """
+from homework_02.base import Vehicle
+from homework_02.exceptions import CargoOverload
+
+
+class Plane(Vehicle):
+    def __init__(self, weight, fuel, fuel_consumption, max_cargo):
+        super().__init__(weight, fuel, fuel_consumption)
+        self.cargo = 0
+        self.max_cargo = max_cargo
+
+    def load_cargo(self, new_cargo):
+        if self.max_cargo >= self.cargo + new_cargo:
+            self.cargo += new_cargo
+        else:
+            raise CargoOverload("Перегрузка")
+
+    def remove_all_cargo(self):
+        before_remove = self.cargo
+        self.cargo = 0
+        return before_remove
