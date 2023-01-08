502f91ad3545fd02069e09c6d1f25e1f94e1f8b7
lisanders <denis-krv93@yandex.ru>
2023-01-08 18:33:17 +0300

    refactor base.py for homework_02

--- homework_02/base.py
+++ homework_02/base.py
@@ -3,15 +3,16 @@ from homework_02.exceptions import LowFuelError, NotEnoughFuel
 
 
 class Vehicle(ABC):
-    weight = 2000
-    started = False
-    fuel = 60
-    fuel_consumption = 12
+    # weight = 2000
+    # started = False
+    # fuel = 60
+    # fuel_consumption = 12
 
-    def __init__(self, weight, fuel, fuel_consumption):
+    def __init__(self, weight=2000, fuel=60, fuel_consumption=12, started=False):  # значения по умолчанию перенесены в init
         self.weight = weight
         self.fuel = fuel
         self.fuel_consumption = fuel_consumption
+        self.started = started
 
     def start(self):
         if not self.started:
@@ -19,7 +20,7 @@ class Vehicle(ABC):
                 self.started = True
             else:
                 raise LowFuelError("Топливо на нуле!")
-        #return self.started  скорее всего что-то лишнее, без него так же работает
+        # return self.started  скорее всего что-то лишнее, без него так же работает
 
     def move(self, distance):
         distance_cost = self.fuel_consumption * distance
@@ -27,5 +28,3 @@ class Vehicle(ABC):
             self.fuel -= distance_cost
         else:
             raise NotEnoughFuel("Топлива не достаточно для преодоления данной дистанции")
-
-
